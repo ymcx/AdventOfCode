307da836880d3b5308fbcc82c25cb86ab5e2fe90
ymcx <89810988+ymcx@users.noreply.github.com>
2025-08-27 17:24:28 +0300

    exercise 3a

--- src/exercise_3.rs
+++ src/exercise_3.rs
@@ -1,5 +1,25 @@
+use crate::misc;
+use regex::Regex;
+
+fn strip_numbers(string: &str) -> usize {
+    string
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse()
+        .unwrap()
+}
+
 pub fn a() -> usize {
-    0
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let text = misc::text();
+
+    re.find_iter(&text)
+        .map(|result| {
+            let (l, r) = result.as_str().split_once(",").unwrap();
+            strip_numbers(l) * strip_numbers(r)
+        })
+        .sum()
 }
 
 pub fn b() -> usize {
--- src/misc.rs
+++ src/misc.rs
@@ -1,16 +1,25 @@
 use std::{
     env,
     fs::File,
-    io::{BufRead, BufReader, Lines},
+    io::{BufRead, BufReader, Lines, Read},
 };
+
+fn get_reader() -> BufReader<File> {
+    let path = args()[2].clone();
+    let file = File::open(path).unwrap();
+    BufReader::new(file)
+}
 
 pub fn args() -> Vec<String> {
     env::args().collect()
 }
 
 pub fn lines() -> Lines<BufReader<File>> {
-    let path = args()[2].clone();
-    let file = File::open(path).unwrap();
-    let reader = BufReader::new(file);
-    reader.lines()
+    get_reader().lines()
+}
+
+pub fn text() -> String {
+    let mut text = String::new();
+    get_reader().read_to_string(&mut text).unwrap();
+    text
 }
