c30494227dc154b8c4e0c18955f308325dbc316d
ymcx <89810988+ymcx@users.noreply.github.com>
2025-08-28 13:12:00 +0300

    exercise 4b

--- src/exercise_4.rs
+++ src/exercise_4.rs
@@ -25,6 +25,30 @@ fn found_word(
     found == word[1..]
 }
 
+fn is_match(left: char, right: char) -> bool {
+    (left == 'M' && right == 'S') || (left == 'S' && right == 'M')
+}
+
+fn found_pair(origin: (isize, isize), board: &Vec<Vec<char>>) -> bool {
+    let (y_max, x_max) = (board.len() as isize, board[0].len() as isize);
+    let (y_origin, x_origin) = origin;
+    let (y1, x1) = (y_origin + 1, x_origin + 1);
+    let (y2, x2) = (y_origin - 1, x_origin + 1);
+    let (y3, x3) = (y_origin - 1, x_origin - 1);
+    let (y4, x4) = (y_origin + 1, x_origin - 1);
+
+    if y3 < 0 || x3 < 0 || y_max <= y1 || x_max <= x1 {
+        return false;
+    }
+
+    let top_right = board[y1 as usize][x1 as usize];
+    let bottom_right = board[y2 as usize][x2 as usize];
+    let bottom_left = board[y3 as usize][x3 as usize];
+    let top_left = board[y4 as usize][x4 as usize];
+
+    is_match(top_right, bottom_left) && is_match(bottom_right, top_left)
+}
+
 pub fn a() -> usize {
     let word = "XMAS";
     let board: Vec<Vec<char>> = misc::text()
@@ -57,5 +81,25 @@ pub fn a() -> usize {
 }
 
 pub fn b() -> usize {
-    0
+    let board: Vec<Vec<char>> = misc::text()
+        .trim()
+        .split("\n")
+        .map(|row| row.chars().collect())
+        .collect();
+
+    let mut count = 0;
+    for (y, row) in board.iter().enumerate() {
+        for (x, char) in row.iter().enumerate() {
+            if *char != 'A' {
+                continue;
+            }
+
+            let origin = (y as isize, x as isize);
+            if found_pair(origin, &board) {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
