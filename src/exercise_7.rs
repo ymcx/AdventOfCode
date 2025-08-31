17bd68615c49cd553e77a7a2ae258f7ab4d48bb2
ymcx <89810988+ymcx@users.noreply.github.com>
2025-08-31 15:59:09 +0300

    exercise 7b

--- src/exercise_7.rs
+++ src/exercise_7.rs
@@ -1,21 +1,46 @@
 use crate::misc;
 
-pub fn a() -> usize {
+fn create_mask(base: usize, seed: usize, length: usize) -> Vec<usize> {
+    let mut value = seed;
+    (0..length)
+        .map(|_| {
+            let bit = value % base;
+            value /= base;
+            bit
+        })
+        .collect()
+}
+
+fn parse_values() -> Vec<(usize, Vec<usize>)> {
     misc::lines()
         .map(|line| {
             let line = line.unwrap();
             let (test, values) = line.split_once(": ").unwrap();
             let test: usize = test.parse().unwrap();
             let values: Vec<usize> = values.split(" ").map(|i| i.parse().unwrap()).collect();
+            (test, values)
+        })
+        .collect()
+}
 
-            let n = values.len() - 1;
+fn get_result(base: usize) -> usize {
+    parse_values()
+        .iter()
+        .map(|(test, values)| {
+            let operator_places = values.len() - 1;
+            let combinations = usize::pow(base, operator_places as u32);
             let mut results = Vec::new();
-            for i in 0..usize::pow(2, n as u32) {
-                let mask: Vec<usize> = (0..n).map(|bit| (i >> bit) & 1).collect();
+            for i in 0..combinations {
+                let mask = create_mask(base, i, operator_places);
                 let mut result = values[0];
                 mask.iter().enumerate().for_each(|(i, value)| match value {
                     0 => result *= values[i + 1],
                     1 => result += values[i + 1],
+                    2 => {
+                        let length = values[i + 1].to_string().len();
+                        let shifted_result = result * usize::pow(10, length as u32);
+                        result = shifted_result + values[i + 1];
+                    }
                     _ => {}
                 });
                 results.push(result);
@@ -25,11 +50,15 @@ pub fn a() -> usize {
                 return test;
             }
 
-            0
+            &0
         })
         .sum()
 }
 
+pub fn a() -> usize {
+    get_result(2)
+}
+
 pub fn b() -> usize {
-    0
+    get_result(3)
 }
