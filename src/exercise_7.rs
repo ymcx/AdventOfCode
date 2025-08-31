a25da87b2c555541abdcc8675fbb8f9cbe2715a4
ymcx <89810988+ymcx@users.noreply.github.com>
2025-08-31 15:23:50 +0300

    exercise 7a

--- src/exercise_7.rs
+++ src/exercise_7.rs
@@ -1,5 +1,33 @@
+use crate::misc;
+
 pub fn a() -> usize {
-    0
+    misc::lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let (test, values) = line.split_once(": ").unwrap();
+            let test: usize = test.parse().unwrap();
+            let values: Vec<usize> = values.split(" ").map(|i| i.parse().unwrap()).collect();
+
+            let n = values.len() - 1;
+            let mut results = Vec::new();
+            for i in 0..usize::pow(2, n as u32) {
+                let mask: Vec<usize> = (0..n).map(|bit| (i >> bit) & 1).collect();
+                let mut result = values[0];
+                mask.iter().enumerate().for_each(|(i, value)| match value {
+                    0 => result *= values[i + 1],
+                    1 => result += values[i + 1],
+                    _ => {}
+                });
+                results.push(result);
+            }
+
+            if results.contains(&test) {
+                return test;
+            }
+
+            0
+        })
+        .sum()
 }
 
 pub fn b() -> usize {
