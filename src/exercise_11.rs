ebfd4e0bcc53ec4895c5351fedc0e78219ae3e60
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-02 11:22:28 +0300

    exercise 11a

--- src/exercise_11.rs
+++ src/exercise_11.rs
@@ -1,5 +1,35 @@
+use crate::misc;
+
+fn do_blink(stones: &Vec<usize>) -> Vec<usize> {
+    let mut new_stones = Vec::new();
+    for i in 0..stones.len() {
+        let stone = stones[i];
+        let stone_str = stone.to_string();
+        let some_stones = if stone == 0 {
+            vec![1]
+        } else if stone_str.len() % 2 == 0 {
+            let (l, r) = stone_str.split_at(stone_str.len() / 2);
+            let (l, r) = (l.parse().unwrap(), r.parse().unwrap());
+            vec![l, r]
+        } else {
+            vec![stone * 2024]
+        };
+
+        new_stones.extend(some_stones);
+    }
+
+    new_stones
+}
+
 pub fn a() -> usize {
-    0
+    let text = misc::text();
+    let mut stones = text.trim().split(" ").map(|i| i.parse().unwrap()).collect();
+
+    for _ in 0..25 {
+        stones = do_blink(&stones);
+    }
+
+    stones.len()
 }
 
 pub fn b() -> usize {
