4faa0a938dded5d6e9d509fd0a64dac425a6ad9a
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-06 21:39:27 +0300

    exercise 14

--- src/exercise_14.rs
+++ src/exercise_14.rs
@@ -1,7 +1,93 @@
+use crate::misc;
+use itertools::Itertools;
+use std::str::Split;
+
+fn parse_values(parts: &mut Split<&str>) -> (i32, i32) {
+    parts.next().unwrap()[2..]
+        .split(",")
+        .map(|i| i.parse().unwrap())
+        .collect_tuple()
+        .unwrap()
+}
+
+fn parse_robots() -> Vec<((i32, i32), (i32, i32))> {
+    misc::lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let mut parts = line.split(" ");
+            (parse_values(&mut parts), parse_values(&mut parts))
+        })
+        .collect()
+}
+
+fn move_robots(robots: &mut Vec<((i32, i32), (i32, i32))>, dimensions: (i32, i32), seconds: i32) {
+    let (x_max, y_max) = dimensions;
+
+    robots.iter_mut().for_each(|(position, velocity)| {
+        *position = (
+            (position.0 + velocity.0 * seconds).rem_euclid(x_max),
+            (position.1 + velocity.1 * seconds).rem_euclid(y_max),
+        );
+    });
+}
+
+fn safe_value(robots: &Vec<((i32, i32), (i32, i32))>, dimensions: (i32, i32)) -> usize {
+    let (x_max, y_max) = dimensions;
+    let (x_mid, y_mid) = (x_max / 2, y_max / 2);
+
+    let mut quadrants = vec![0; 4];
+    robots.iter().for_each(|&((x, y), _)| {
+        if x == x_mid || y == y_mid {
+            return;
+        }
+
+        let mut quadrant = 0;
+        if x < x_mid {
+            quadrant += 1;
+        }
+        if y < y_mid {
+            quadrant += 2;
+        }
+        quadrants[quadrant] += 1;
+    });
+
+    quadrants.iter().product()
+}
+
+fn contains_consecutive_columns(robots: &Vec<((i32, i32), (i32, i32))>) -> bool {
+    let positions: Vec<(i32, i32)> = robots.iter().map(|&(position, _)| position).collect();
+    positions
+        .iter()
+        .filter(|&&position| {
+            vec![position; 7]
+                .iter()
+                .enumerate()
+                .map(|(i, &(x, y))| (x, y + i as i32))
+                .all(|position| positions.contains(&position))
+        })
+        .count()
+        != 0
+}
+
 pub fn a() -> usize {
-    0
+    let mut robots = parse_robots();
+    let dimensions = (101, 103);
+
+    move_robots(&mut robots, dimensions, 100);
+    safe_value(&robots, dimensions)
 }
 
 pub fn b() -> usize {
+    let mut robots = parse_robots();
+    let dimensions = (101, 103);
+
+    for i in 0..10000 {
+        if contains_consecutive_columns(&robots) {
+            return i;
+        }
+
+        move_robots(&mut robots, dimensions, 1);
+    }
+
     0
 }
