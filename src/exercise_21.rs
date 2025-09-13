6f744e835dce79bef8ef83e5bf5c348a99843f09
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-13 18:52:25 +0300

    exercise 21a

--- src/exercise_21.rs
+++ src/exercise_21.rs
@@ -1,23 +1,21 @@
 use crate::misc::{self, SignedPoint};
 
-fn add_inputs(delta: SignedPoint, inputs: &mut Vec<char>, vertical: bool) {
-    let (delta_n, letters) = if vertical {
-        (delta.1, ('^', 'v'))
+fn add_inputs((dx, dy): SignedPoint, inputs: &mut Vec<char>, vertical_first: bool) {
+    let letters_horizontal = vec![if dx.is_negative() { '<' } else { '>' }; dx.abs() as usize];
+    let letters_vertical = vec![if dy.is_negative() { '^' } else { 'v' }; dy.abs() as usize];
+
+    if vertical_first {
+        inputs.extend(letters_vertical);
+        inputs.extend(letters_horizontal);
     } else {
-        (delta.0, ('<', '>'))
-    };
-    for _ in 0..delta_n.abs() {
-        if delta_n < 0 {
-            inputs.push(letters.0);
-        } else {
-            inputs.push(letters.1);
-        }
+        inputs.extend(letters_horizontal);
+        inputs.extend(letters_vertical);
     }
+    inputs.push('A');
 }
 
-fn get_coordinates_numeric(char: char) -> (SignedPoint, SignedPoint) {
-    let death_point = (0, 3);
-    let point = match char {
+fn get_coordinates_numeric(char: char) -> SignedPoint {
+    match char {
         '7' => (0, 0),
         '8' => (1, 0),
         '9' => (2, 0),
@@ -30,71 +28,70 @@ fn get_coordinates_numeric(char: char) -> (SignedPoint, SignedPoint) {
         '0' => (1, 3),
         'A' => (2, 3),
         _ => panic!(),
-    };
-    (point, death_point)
+    }
 }
-fn get_coordinates_directional(char: char) -> (SignedPoint, SignedPoint) {
-    let death_point = (0, 0);
-    let point = match char {
+fn get_coordinates_directional(char: char) -> SignedPoint {
+    match char {
         '^' => (1, 0),
         'A' => (2, 0),
         '<' => (0, 1),
         'v' => (1, 1),
         '>' => (2, 1),
         _ => panic!(),
-    };
-    (point, death_point)
+    }
 }
 
-fn keypad(char: char, current_point: &mut SignedPoint, numeric: bool) -> Vec<char> {
-    let (char_point, death_point) = if numeric {
+fn keypad(char: char, (x, y): &mut SignedPoint, numeric: bool) -> Vec<char> {
+    let mut inputs = Vec::new();
+    let (next_x, next_y) = if numeric {
         get_coordinates_numeric(char)
     } else {
         get_coordinates_directional(char)
     };
+    let delta = (next_x - *x, next_y - *y);
 
-    let delta = (
-        char_point.0 - current_point.0,
-        char_point.1 - current_point.1,
-    );
-
-    let mut inputs = Vec::new();
-    if (char_point.0, current_point.1) == death_point {
+    // https://www.reddit.com/r/adventofcode/comments/1hjgyps/2024_day_21_part_2_i_got_greedyish/
+    if (numeric && *x == 0 && next_y == 3) || (!numeric && *x == 0 && next_y == 0) {
+        add_inputs(delta, &mut inputs, false);
+    } else if (numeric && *y == 3 && next_x == 0) || (!numeric && *y == 0 && next_x == 0) {
         add_inputs(delta, &mut inputs, true);
+    } else if delta.0 < 0 {
         add_inputs(delta, &mut inputs, false);
     } else {
-        add_inputs(delta, &mut inputs, false);
         add_inputs(delta, &mut inputs, true);
     }
-    inputs.push('A');
-    *current_point = char_point;
+
+    (*x, *y) = (next_x, next_y);
 
     inputs
 }
 
-pub fn a(path: &str) -> String {
+fn line_to_complexity(line: &str) -> usize {
     let mut robots = ((2, 3), (2, 0), (2, 0));
+    let movements: String = line
+        .chars()
+        .map(|char| keypad(char, &mut robots.0, true))
+        .flatten()
+        .map(|char| keypad(char, &mut robots.1, false))
+        .flatten()
+        .map(|char| keypad(char, &mut robots.2, false))
+        .flatten()
+        .collect();
+    let len = movements.len();
+    let num: usize = line
+        .chars()
+        .filter(|char| char.is_digit(10))
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    len * num
+}
+
+pub fn a(path: &str) -> String {
     misc::lines(path)
         .iter()
-        .map(|line| {
-            let movements: String = line
-                .chars()
-                .map(|char| keypad(char, &mut robots.0, true))
-                .flatten()
-                .map(|char| keypad(char, &mut robots.1, false))
-                .flatten()
-                .map(|char| keypad(char, &mut robots.2, false))
-                .flatten()
-                .collect();
-            let len = movements.len();
-            let num: usize = line
-                .chars()
-                .filter(|char| char.is_digit(10))
-                .collect::<String>()
-                .parse()
-                .unwrap();
-            len * num
-        })
+        .map(|line| line_to_complexity(line))
         .sum::<usize>()
         .to_string()
 }
@@ -103,11 +100,8 @@ pub fn b(_path: &str) -> String {
     "".to_string()
 }
 
-// #[test]
-// fn test() {
-//     assert!(a("input/exercise_21.txt") != 0);
-//     assert!(b("input/exercise_21.txt") != 0);
-// }
-
-// 179008 wrong
-// 180372 high
+#[test]
+fn test() {
+    assert!(a("input/exercise_21.txt") == "174124");
+    assert!(b("input/exercise_21.txt") != "");
+}
