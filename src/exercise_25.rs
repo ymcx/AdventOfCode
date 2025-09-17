afb68136e1f14a75fd1a4239879446ad01c73998
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-17 13:59:09 +0300

    exercise 25a

--- src/exercise_25.rs
+++ src/exercise_25.rs
@@ -1,13 +1,48 @@
-pub fn a(_path: &str) -> String {
-    "".to_string()
+use crate::misc;
+
+fn parse(path: &str) -> (Vec<[usize; 5]>, Vec<[usize; 5]>) {
+    let (mut locks, mut keys) = (Vec::new(), Vec::new());
+
+    misc::text(path).split("\n\n").for_each(|schematic| {
+        let mut item = [0; 5];
+        for line in schematic.lines().skip(1).take(5) {
+            for (i, char) in line.chars().enumerate() {
+                if char == '#' {
+                    item[i] += 1;
+                }
+            }
+        }
+
+        let is_lock = schematic.chars().next().unwrap() == '#';
+        if is_lock {
+            locks.push(item);
+        } else {
+            keys.push(item);
+        }
+    });
+
+    (locks, keys)
+}
+pub fn a(path: &str) -> String {
+    let (locks, keys) = parse(path);
+
+    locks
+        .iter()
+        .map(|lock| {
+            keys.iter()
+                .filter(|key| (0..5).all(|i| key[i] + lock[i] <= 5))
+                .count()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 pub fn b(_path: &str) -> String {
     "".to_string()
 }
 
-// #[test]
-// fn test() {
-//     assert!(a("input/exercise_25.txt") != 0);
-//     assert!(b("input/exercise_25.txt") != 0);
-// }
+#[test]
+fn test() {
+    assert!(a("input/exercise_25.txt") == "3196");
+    // assert!(b("input/exercise_25.txt") != 0);
+}
