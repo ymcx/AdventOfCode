e810ef6d0fc161055ea154fa2985a8cf0bef4018
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-17 16:59:15 +0300

    exercise 21b

--- src/exercise_21.rs
+++ src/exercise_21.rs
@@ -1,5 +1,5 @@
 use crate::misc::{self, SignedPoint};
-use std::{collections::HashMap, str::Chars};
+use std::collections::HashMap;
 
 const PATH: [[&str; 5]; 5] = [
     ["A", ">^A", ">A", ">>^A", ">>A"],
@@ -9,41 +9,41 @@ const PATH: [[&str; 5]; 5] = [
     ["<<A", "<^A", "<A", "^A", "A"],
 ];
 
-const fn get_index_directional(char: u8) -> usize {
+const fn get_index_directional(char: char) -> usize {
     match char {
-        b'<' => 0,
-        b'^' => 1,
-        b'v' => 2,
-        b'A' => 3,
-        b'>' => 4,
+        '<' => 0,
+        '^' => 1,
+        'v' => 2,
+        'A' => 3,
+        '>' => 4,
         _ => panic!(),
     }
 }
 
-const fn get_coordinates_directional(char: u8) -> SignedPoint {
+const fn get_coordinates_directional(char: char) -> SignedPoint {
     match char {
-        b'^' => (1, 0),
-        b'A' => (2, 0),
-        b'<' => (0, 1),
-        b'v' => (1, 1),
-        b'>' => (2, 1),
+        '^' => (1, 0),
+        'A' => (2, 0),
+        '<' => (0, 1),
+        'v' => (1, 1),
+        '>' => (2, 1),
         _ => panic!(),
     }
 }
 
-const fn get_coordinates_numeric(char: u8) -> SignedPoint {
+const fn get_coordinates_numeric(char: char) -> SignedPoint {
     match char {
-        b'7' => (0, 0),
-        b'8' => (1, 0),
-        b'9' => (2, 0),
-        b'4' => (0, 1),
-        b'5' => (1, 1),
-        b'6' => (2, 1),
-        b'1' => (0, 2),
-        b'2' => (1, 2),
-        b'3' => (2, 2),
-        b'0' => (1, 3),
-        b'A' => (2, 3),
+        '7' => (0, 0),
+        '8' => (1, 0),
+        '9' => (2, 0),
+        '4' => (0, 1),
+        '5' => (1, 1),
+        '6' => (2, 1),
+        '1' => (0, 2),
+        '2' => (1, 2),
+        '3' => (2, 2),
+        '0' => (1, 3),
+        'A' => (2, 3),
         _ => panic!(),
     }
 }
@@ -62,7 +62,7 @@ fn add_inputs((dx, dy): SignedPoint, inputs: &mut Vec<char>, vertical_first: boo
     inputs.push('A');
 }
 
-fn numeric_keypad(char: u8, (x, y): &mut SignedPoint) -> Vec<char> {
+fn numeric_keypad(char: char, (x, y): &mut SignedPoint) -> Vec<char> {
     let mut inputs = Vec::new();
     let (next_x, next_y) = get_coordinates_numeric(char);
     let delta = (next_x - *x, next_y - *y);
@@ -76,53 +76,41 @@ fn numeric_keypad(char: u8, (x, y): &mut SignedPoint) -> Vec<char> {
     inputs
 }
 
-fn directional_keypad(
-    char: u8,
-    (x, y): &mut SignedPoint,
-    old_index: &mut usize,
-    index: &mut usize,
-) -> Chars<'static> {
-    *old_index = *index;
-    *index = get_index_directional(char);
-
-    let (next_x, next_y) = get_coordinates_directional(char);
-    (*x, *y) = (next_x, next_y);
-
-    PATH[*old_index][*index].chars()
-}
-
-fn line_to_complexity(line: &str, n: usize, cache: &mut HashMap<String, Vec<char>>) -> usize {
-    let mut robots = ((2, 3), (2, 0));
-    let mut movements: String = line
-        .chars()
-        .map(|c| numeric_keypad(c as u8, &mut robots.0))
-        .flatten()
-        .collect();
-
-    let mut index = 3;
-    let mut old_index = index;
-    for _ in 0..n {
-        movements = movements
-            .split_inclusive('A')
-            .map(|part| {
-                if let Some(cached) = cache.get(part) {
-                    return cached.clone();
-                }
-
-                let result: Vec<_> = part
-                    .chars()
-                    .map(|c| directional_keypad(c as u8, &mut robots.1, &mut old_index, &mut index))
-                    .flatten()
-                    .collect();
-
-                cache.insert(part.to_string(), result.clone());
-                result
-            })
-            .flatten()
-            .collect()
+fn split(movements: &str, n: usize, cache: &mut HashMap<(String, usize), usize>) -> usize {
+    if n == 0 {
+        return movements.len();
     }
 
-    let len = movements.len();
+    let cache_key = (movements.to_string(), n);
+    if let Some(&cached) = cache.get(&cache_key) {
+        return cached;
+    }
+
+    let mut old = 3;
+    let mut cur = 3;
+    let mut robot = (2, 0);
+    let sum = movements
+        .chars()
+        .map(|c| {
+            old = cur;
+            cur = get_index_directional(c);
+            robot = get_coordinates_directional(c);
+            split(PATH[old][cur], n - 1, cache)
+        })
+        .sum();
+
+    cache.insert(cache_key, sum);
+    sum
+}
+
+fn line_to_complexity(line: &str, n: usize, cache: &mut HashMap<(String, usize), usize>) -> usize {
+    let mut robot = (2, 3);
+    let movements: String = line
+        .chars()
+        .flat_map(|c| numeric_keypad(c, &mut robot))
+        .collect();
+
+    let len = split(&movements, n, cache);
     let num: usize = line
         .chars()
         .filter(|char| char.is_digit(10))
@@ -147,11 +135,11 @@ pub fn a(path: &str) -> String {
 }
 
 pub fn b(path: &str) -> String {
-    solve(path, 20)
+    solve(path, 25)
 }
 
 #[test]
 fn test() {
     assert!(a("input/exercise_21.txt") == "174124");
-    // assert!(b("input/exercise_21.txt") != "");
+    assert!(b("input/exercise_21.txt") == "216668579770346");
 }
