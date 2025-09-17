f7d95215580b33a05b099d73fd9f958b4d201804
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-17 15:29:15 +0300

    exercise 22b

--- src/exercise_22.rs
+++ src/exercise_22.rs
@@ -1,31 +1,56 @@
 use crate::misc;
+use std::collections::{HashMap, HashSet};
 
-fn get_secret(line: &str, n: usize) -> usize {
-    let mut secret = line.parse().unwrap();
-    let modulo = 16777216;
+fn get_secret(mut secret: usize) -> usize {
+    secret = (secret * 64 ^ secret) % 16777216;
+    secret = (secret / 32 ^ secret) % 16777216;
+    secret = (secret * 2048 ^ secret) % 16777216;
+    secret
+}
+
+fn get_secret_n(mut secret: usize, n: usize) -> usize {
     for _ in 0..n {
-        secret = ((secret * 64) ^ secret) % modulo;
-        secret = ((secret / 32) ^ secret) % modulo;
-        secret = ((secret * 2048) ^ secret) % modulo;
+        secret = get_secret(secret)
     }
-
     secret
 }
 
 pub fn a(path: &str) -> String {
     misc::lines(path)
         .iter()
-        .map(|line| get_secret(line, 2000))
+        .map(|line| get_secret_n(line.parse().unwrap(), 2000))
         .sum::<usize>()
         .to_string()
 }
 
-pub fn b(_path: &str) -> String {
-    "".to_string()
+pub fn b(path: &str) -> String {
+    let mut sequences = HashMap::new();
+    for line in misc::lines(path) {
+        let mut secret = line.parse().unwrap();
+        let mut secret_char = secret % 10;
+        let mut seen = HashSet::new();
+        let mut sequence = [0; 4];
+
+        for i in 0..2000 {
+            secret = get_secret(secret);
+            sequence.rotate_left(1);
+            sequence[3] = (secret % 10) as i32 - secret_char as i32;
+            secret_char = secret % 10;
+
+            if seen.contains(&sequence) || i < 3 {
+                continue;
+            }
+
+            seen.insert(sequence);
+            *sequences.entry(sequence).or_insert(0) += secret_char;
+        }
+    }
+
+    sequences.values().max().unwrap().to_string()
 }
 
 #[test]
 fn test() {
     assert!(a("input/exercise_22.txt") == "20071921341");
-    // assert!(b("input/exercise_22.txt") != 0);
+    assert!(b("input/exercise_22.txt") == "2242");
 }
