047675b8d6368994f5dae006d9c241a3c334de68
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-28 00:13:08 +0300

    2023, day5b

--- 2023/src/day05.cpp
+++ 2023/src/day05.cpp
@@ -7,67 +7,88 @@
 
 using namespace std;
 
-tuple<vector<long>, vector<vector<tuple<long, long, long>>>>
+pair<vector<long>, vector<vector<tuple<long, long, long>>>>
 parse_seeds(string path) {
   vector<string> lines = read_lines(path);
   string seeds_str = regex_replace(lines[0], regex(".+: "), "");
   vector<string> seeds_vec = split(seeds_str, " ");
   vector<long> seeds = filter_map(seeds_vec, parse_long);
 
+  int i = -1;
   vector<vector<tuple<long, long, long>>> maps;
-  maps.resize(40);
-  long i = 0;
-  for (long ln = 3; ln < lines.size(); ++ln) {
-    string line = lines[ln];
+  maps.resize(7);
+
+  for (string line : lines) {
     if (line.empty()) {
+      ++i;
       continue;
     }
-    if (line.contains('m')) {
-      i += 1;
+
+    if (line.contains(':')) {
       continue;
     }
-    vector<string> strings = split(line, " ");
-    vector<long> nums = filter_map(strings, parse_long);
-    tuple<long, long, long> t = {nums[0], nums[1], nums[2]};
-    maps[i].push_back(t);
+
+    vector<string> tokens = split(line, " ");
+    vector<long> values = filter_map(tokens, parse_long);
+    maps[i].emplace_back(values[0], values[1], values[2]);
   }
 
   return {seeds, maps};
 }
 
-tuple<long, bool> convert(long input, tuple<long, long, long> range) {
-  long dst = get<0>(range);
-  long src = get<1>(range);
-  long len = get<2>(range);
-
-  if (input < src || src + len <= input) {
-    return {input, false};
+vector<pair<long, long>> parse_ranges(const vector<long> &seeds) {
+  vector<pair<long, long>> seed_ranges;
+  for (int i = 0; i < seeds.size() / 2; ++i) {
+    long start = seeds[i * 2];
+    long len = seeds[i * 2 + 1];
+    seed_ranges.emplace_back(start, start + len);
   }
 
-  long delta = dst - src;
-  return {input + delta, true};
+  return seed_ranges;
+}
+
+long reverse_map(long location,
+                 const vector<vector<tuple<long, long, long>>> &maps) {
+  for (int i = maps.size() - 1; i >= 0; --i) {
+    for (auto [dst, src, len] : maps[i]) {
+      if (dst <= location && location < dst + len) {
+        location += src - dst;
+        break;
+      }
+    }
+  }
+
+  return location;
+}
+
+bool is_seed(long seed, const vector<pair<long, long>> &seed_ranges) {
+  for (auto [min, max] : seed_ranges) {
+    if (min <= seed && seed < max) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 int main(int argc, char *argv[]) {
   auto [seeds, maps] = parse_seeds(argv[1]);
+  vector<pair<long, long>> seed_ranges = parse_ranges(seeds);
+  long seed = 0, location = 0, p1 = 0, p2 = 0;
 
-  long smallest = -1;
-  for (long seed : seeds) {
-    for (auto map : maps) {
-      for (tuple<long, long, long> t : map) {
-        auto [newseed, converted] = convert(seed, t);
-        if (converted) {
-          seed = newseed;
-          break;
-        }
-      }
+  while (!p1 || !p2) {
+    seed = reverse_map(location, maps);
+
+    if (!p1 && contains(seeds, seed)) {
+      p1 = location;
     }
-    if (smallest == -1) {
-      smallest = seed;
+
+    if (!p2 && is_seed(seed, seed_ranges)) {
+      p2 = location;
     }
-    if (seed < smallest) {
-      smallest = seed;
-    }
+
+    ++location;
   }
-  println(smallest);
+
+  assert_print(p1, p2, 484023871, 46294175);
 }
