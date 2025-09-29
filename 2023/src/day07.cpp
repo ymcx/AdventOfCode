5a63b4c7fe03ebebbfdb7d865eac6f490a19a6d3
ymcx <89810988+ymcx@users.noreply.github.com>
2025-09-29 22:54:37 +0300

    2023, day7b

--- 2023/src/day07.cpp
+++ 2023/src/day07.cpp
@@ -8,73 +8,106 @@
 
 using namespace std;
 
-int label(string cards) {
-  unordered_map<char, int> map;
-  for (char c : cards) {
-    ++map[c];
-  }
+unordered_map<char, int> p1_priority = {
+    {'A', 12}, {'K', 11}, {'Q', 10}, {'J', 9}, {'T', 8}, {'9', 7}, {'8', 6},
+    {'7', 5},  {'6', 4},  {'5', 3},  {'4', 2}, {'3', 1}, {'2', 0},
+};
 
-  if (map.size() == 1) {
-    return 1;
-  }
-  if (map.size() == 4) {
-    return 6;
-  }
-  if (map.size() == 5) {
-    return 7;
-  }
+unordered_map<char, int> p2_priority = {
+    {'A', 12}, {'K', 11}, {'Q', 10}, {'J', -1}, {'T', 8}, {'9', 7}, {'8', 6},
+    {'7', 5},  {'6', 4},  {'5', 3},  {'4', 2},  {'3', 1}, {'2', 0},
+};
 
-  if (map.size() == 2) {
-    vector<int> vals = values(map);
-    if (vals[0] == 4 || vals[0] == 1) {
-      return 2;
-    } else {
-      return 3;
+char max_key(const unordered_map<char, int> &map) {
+  char max_key = ' ';
+  int max_value = 0;
+  for (auto [key, value] : map) {
+    if (max_value < value) {
+      max_value = value;
+      max_key = key;
     }
   }
 
-  int two = 2;
-  if (contains(values(map), two)) {
-    return 5;
-  }
-  return 4;
+  return max_key;
 }
 
-unordered_map<char, int> char_priority = {
-    {'A', 0}, {'K', 1}, {'Q', 2}, {'J', 3},  {'T', 4},  {'9', 5}, {'8', 6},
-    {'7', 7}, {'6', 8}, {'5', 9}, {'4', 10}, {'3', 11}, {'2', 12}};
-
-bool custom_compare(const std::string &a, const std::string &b) {
-  for (int i = 0; i < 5; ++i) {
-    int pa = char_priority[a[i]];
-    int pb = char_priority[b[i]];
-    if (pa != pb)
-      return pa > pb;
+unordered_map<char, int> char_freq(string input, bool p2) {
+  unordered_map<char, int> freq;
+  for (char c : input) {
+    ++freq[c];
   }
+
+  if (p2) {
+    freq.erase('J');
+    char max = max_key(freq);
+    freq[max] += ranges::count(input, 'J');
+  }
+
+  return freq;
+}
+
+int get_group(string cards, bool p2) {
+  unordered_map<char, int> freq = char_freq(cards, p2);
+  switch (freq.size()) {
+  case (5):
+    return 6;
+  case (4):
+    return 5;
+  case (3):
+    return (contains(values(freq), 3)) ? 3 : 4;
+  case (2):
+    return (contains(values(freq), 1)) ? 1 : 2;
+  }
+
+  return 0;
+}
+
+bool compare(string a, string b, const unordered_map<char, int> &priority) {
+  for (int i = 0; i < 5; ++i) {
+    int pa = priority.at(a[i]);
+    int pb = priority.at(b[i]);
+    if (pa != pb) {
+      return pa < pb;
+    }
+  }
+
   return true;
+}
+
+int solve(const vector<string> &lines, bool p2) {
+  unordered_map<int, vector<string>> groups;
+  unordered_map<string, int> weights;
+
+  for (string line : lines) {
+    vector<string> parts = split(line, " ");
+
+    string cards = parts[0];
+    weights[cards] = stoi(parts[1]);
+
+    int group = get_group(cards, p2);
+    groups[group].push_back(cards);
+  }
+
+  unordered_map<char, int> priority = p2 ? p2_priority : p1_priority;
+  int result = 0;
+  int rank = 0;
+
+  for (int i = 6; i >= 0; --i) {
+    vector<string> group = groups[i];
+    sort(group.begin(), group.end(),
+         [&](string a, string b) { return compare(a, b, priority); });
+
+    for (string cards : group) {
+      result += weights[cards] * ++rank;
+    }
+  }
+
+  return result;
 }
 
 int main(int argc, char *argv[]) {
   vector<string> lines = read_lines(argv[1]);
-  unordered_map<int, vector<string>> map;
-
-  for (string line : lines) {
-    string str = split(line, " ")[0];
-    int l = label(str) - 1;
-    map[l].push_back(line);
-  }
-
-  int results = 0;
-
-  int x = 1;
-  for (int i = 6; i >= 0; --i) {
-    vector<string> vals = map[i];
-    std::sort(vals.begin(), vals.end(), custom_compare);
-    for (string v : vals) {
-      results += stoi(split(v, " ")[1]) * x;
-      ++x;
-    }
-  }
-
-  println(results);
+  int p1 = solve(lines, false);
+  int p2 = solve(lines, true);
+  assert_print(p1, p2, 246424613, 248256639);
 }
