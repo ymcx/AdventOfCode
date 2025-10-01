1d1dbe904d0380e659b367de25b93bb009c2ef81
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-02 00:37:44 +0300

    2023, day10b

--- 2023/src/day10.cpp
+++ 2023/src/day10.cpp
@@ -1,117 +1,146 @@
 #include "misc/io.h"
 #include "misc/point.h"
-#include <regex>
+#include <cstring>
+#include <ranges>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-Point get_next(Point point, Point dp, const vector<vector<char>> &map) {
-  int max_y = map.size();
-  int max_x = map[0].size();
+Point get_next_delta(const vector<vector<char>> &map, Point current,
+                     Point delta) {
+  Point up = Point(-1, 0);
+  Point down = Point(1, 0);
+  Point left = Point(0, -1);
+  Point right = Point(0, 1);
 
-  char item = map[point.y][point.x];
-  char right, left, up, down;
-  switch (item) {
-  case ('S'):
-    if (point.x + 1 < max_x) {
-      right = map[point.y][point.x + 1];
-      if (right == '-' || right == 'J' || right == '7') {
-        return Point(0, 1);
-      }
-    }
-    if (point.x - 1 >= 0) {
-      left = map[point.y][point.x - 1];
-      if (left == '-' || left == 'F' || left == 'L') {
-        return Point(0, -1);
-      }
-    }
-    if (point.y - 1 >= 0) {
-      up = map[point.y - 1][point.x];
-      if (up == '|' || up == 'F' || up == '7') {
-        return Point(-1, 0);
-      }
-    }
-    if (point.y + 1 < max_y) {
-      down = map[point.y + 1][point.x];
-      if (down == '|' || down == 'J' || down == 'L') {
-        return Point(1, 0);
-      }
-    }
+  switch (map[current.y][current.x]) {
   case ('|'):
-    if (dp == Point(-1, 0)) {
-      return Point(-1, 0);
-    } else {
-      return Point(1, 0);
-    }
+    return (delta == up) ? up : down;
   case ('-'):
-    if (dp == Point(0, 1)) {
-      return Point(0, 1);
-    } else {
-      return Point(0, -1);
-    }
+    return (delta == right) ? right : left;
   case ('F'):
-    if (dp == Point(-1, 0)) {
-      return Point(0, 1);
-    } else {
-      return Point(1, 0);
-    }
-  case ('J'):
-    if (dp == Point(0, 1)) {
-      return Point(-1, 0);
-    } else {
-      return Point(0, -1);
-    }
+    return (delta == up) ? right : down;
   case ('L'):
-    if (dp == Point(1, 0)) {
-      return Point(0, 1);
-    } else {
-      return Point(-1, 0);
-    }
+    return (delta == down) ? right : up;
   case ('7'):
-    if (dp == Point(-1, 0)) {
-      return Point(0, -1);
-    } else {
-      return Point(1, 0);
-    }
+    return (delta == right) ? down : left;
+  case ('J'):
+    return (delta == right) ? up : left;
   }
+
   return Point(0, 0);
 }
 
-void check_grid(const vector<vector<char>> &map, Point starting_point) {
-  int max_y = map.size();
-  int max_x = map[0].size();
+unordered_set<Point> get_path(const vector<vector<char>> &map, Point start) {
+  unordered_set<Point> path;
+  Point current = start;
+  Point delta;
 
-  Point cur = starting_point;
-  Point lastd = Point(0, 0);
+  do {
+    Point next_delta = get_next_delta(map, current, delta);
+    delta = next_delta;
+    current = current + next_delta;
+    path.insert(current);
+  } while (current != start);
 
-  int i = 0;
-  while (true) {
-    Point nextd = get_next(cur, lastd, map);
-    lastd = nextd;
-    cur = cur + nextd;
+  return path;
+}
 
-    if (map[cur.y][cur.x] == 'S') {
-      break;
+int get_inside_area(const vector<vector<char>> &map,
+                    const unordered_set<Point> &path) {
+  unordered_set<Point> inside_area;
+
+  for (auto [y, line] : views::enumerate(map)) {
+    bool inside = false;
+    char last;
+
+    for (auto [x, item] : views::enumerate(line)) {
+      Point current = Point(y, x);
+
+      if (path.contains(current)) {
+        if ((item == 'J' && last == 'F') || (item == '7' && last == 'L') ||
+            (item == '|')) {
+          inside ^= true;
+        }
+
+        if (strchr("F7JL|", item)) {
+          last = item;
+        }
+
+        continue;
+      }
+
+      if (inside) {
+        inside_area.insert(current);
+      }
     }
-    ++i;
   }
-  println((i + 1) / 2);
+
+  return inside_area.size();
+}
+
+vector<vector<char>> transform_chars(const vector<string> &input) {
+  vector<vector<char>> output;
+  for (string i : input) {
+    output.emplace_back(i.begin(), i.end());
+  }
+
+  return output;
+}
+
+Point get_start(const vector<vector<char>> &map) {
+  for (auto [y, line] : views::enumerate(map)) {
+    for (auto [x, item] : views::enumerate(line)) {
+      if (item == 'S') {
+        return Point(y, x);
+      }
+    }
+  }
+
+  return Point(0, 0);
+}
+
+bool connects_left(char c) { return strchr("L-F", c); }
+
+bool connects_right(char c) { return strchr("J-7", c); }
+
+bool connects_up(char c) { return strchr("F|7", c); }
+
+bool connects_down(char c) { return strchr("J|L", c); }
+
+char determine_pipe(const vector<vector<char>> &map, Point point) {
+  char left = map[point.y][point.x - 1];
+  char right = map[point.y][point.x + 1];
+  char up = map[point.y - 1][point.x];
+  char down = map[point.y + 1][point.x];
+
+  if (connects_left(left) && connects_right(right))
+    return '-';
+  if (connects_up(up) && connects_down(down))
+    return '|';
+  if (connects_left(left) && connects_up(up))
+    return 'J';
+  if (connects_left(left) && connects_down(down))
+    return '7';
+  if (connects_right(right) && connects_up(up))
+    return 'L';
+  if (connects_right(right) && connects_down(down))
+    return 'F';
+
+  return ' ';
 }
 
 int main(int argc, char *argv[]) {
   vector<string> lines = read_lines(argv[1]);
-  Point starting_point;
-  vector<vector<char>> map;
+  vector<vector<char>> map = transform_chars(lines);
+  Point start = get_start(map);
+  map[start.y][start.x] = determine_pipe(map, start);
+  unordered_set<Point> path = get_path(map, start);
 
-  for (auto [y, line] : views::enumerate(lines)) {
-    map.emplace_back(line.begin(), line.end());
+  int p1 = path.size() / 2;
+  int p2 = get_inside_area(map, path);
 
-    for (auto [x, item] : views::enumerate(line)) {
-      if (item == 'S') {
-        starting_point = Point(y, x);
-      }
-    }
-  }
-  check_grid(map, starting_point);
+  assert_print(p1, p2, 6903, 265);
 }
