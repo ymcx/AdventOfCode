bea84a335191ef531ebbb7592948909ed30e735a
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-13 16:13:10 +0300

    2023, day18b

--- 2023/src/day18.cpp
+++ 2023/src/day18.cpp
@@ -1,49 +1,76 @@
 #include "misc/io.h"
 #include "misc/point.h"
 #include "misc/string.h"
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  vector<string> lines = read_lines(argv[1]);
+int get_direction(char direction) {
+  switch (direction) {
+  case 'R':
+  case '0':
+    return RIGHT;
+  case 'D':
+  case '1':
+    return DOWN;
+  case 'L':
+  case '2':
+    return LEFT;
+  case 'U':
+  case '3':
+    return UP;
+  default:
+    return -1;
+  }
+}
 
+vector<DirectionalPoint> get_points(const vector<string> &lines, bool p2) {
   DirectionalPoint point = DirectionalPoint();
-  int direction = 0;
-  int total = 0;
-
   vector<DirectionalPoint> points;
-  points.push_back(point);
 
-  for (const string &line : lines) {
-    switch (line[0]) {
-    case 'L':
-      direction = LEFT;
-      break;
-    case 'R':
-      direction = RIGHT;
-      break;
-    case 'U':
-      direction = UP;
-      break;
-    case 'D':
-      direction = DOWN;
-      break;
-    }
+  for (string line : lines) {
+    vector<string> parts = split_space(line);
 
-    int steps = stoi(split_space(line)[1]);
+    char direction_char = p2 ? parts[2][7] : parts[0][0];
+    int direction = get_direction(direction_char);
 
-    for (int d = 0; d < steps; ++d) {
+    string steps_string = p2 ? parts[2].substr(2, 5) : parts[1];
+    int base = p2 ? 16 : 10;
+    int steps = stoi(steps_string, 0, base);
+
+    for (int i = 0; i < steps; ++i) {
       point.move_inplace(direction);
       points.push_back(point);
     }
   }
-  for (int i = 1; i < points.size(); ++i) {
-    total += points[i - 1].x * points[i].y - points[i].x * points[i - 1].y;
-  }
-  total +=
-      points.back().x * points.front().y - points.front().x * points.back().y;
 
-  println(total / 2 + points.size() / 2 + 1);
+  return points;
+}
+
+long calculate_area(const vector<DirectionalPoint> &points) {
+  long area = 0;
+
+  for (int i = 1; i < points.size(); ++i) {
+    area += points[i - 1].x * points[i].y - points[i].x * points[i - 1].y;
+  }
+
+  area += points.back().x * points[0].y - points[0].x * points.back().y;
+  area += points.size();
+  area /= 2;
+  area += 1;
+
+  return area;
+}
+
+int main(int argc, char *argv[]) {
+  vector<string> lines = read_lines(argv[1]);
+  vector<DirectionalPoint> points_p1 = get_points(lines, false);
+  vector<DirectionalPoint> points_p2 = get_points(lines, true);
+
+  long p1 = calculate_area(points_p1);
+  long p2 = calculate_area(points_p2);
+
+  assert_print(p1, p2, 46359L, 59574883048274L);
 }
