bbcb70bd3cfcc331c08899ff71db5ab8de3b1780
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-14 11:59:28 +0300

    2023, day19b

--- 2023/src/day19.cpp
+++ 2023/src/day19.cpp
@@ -1,113 +1,105 @@
 #include "misc/io.h"
 #include "misc/string.h"
 #include "misc/vector.h"
+#include <algorithm>
+#include <cstring>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 using namespace std;
+using Workflow = vector<tuple<char, bool, int, string>>;
+using Workflows = unordered_map<string, Workflow>;
+using Rating = vector<long>;
+using Ratings = vector<Rating>;
 
-pair<unordered_map<string, vector<tuple<char, bool, int, string>>>,
-     vector<vector<int>>>
-parse_instructions(string path) {
-  string text = read_text(path);
-  vector<string> parts = split_newnewline(text);
+Workflows parse_workflows(string input) {
+  Workflows workflows;
 
-  unordered_map<string, vector<tuple<char, bool, int, string>>> workflows;
-  for (string workflow : split_newline(parts[0])) {
+  for (string workflow : split_newline(input)) {
     vector<string> workflow_parts = split(workflow, "{");
-    workflow = workflow_parts[0];
-    string instructions =
-        workflow_parts[1].substr(0, workflow_parts[1].length() - 1);
+    string key = workflow_parts[0];
+    string instructions = workflow_parts[1];
+    instructions.pop_back();
 
     for (string instruction : split_comma(instructions)) {
-      vector<string> p = split(instruction, ":");
-      if (p.size() == 1) {
-        workflows[workflow].push_back({' ', false, -1, p[0]});
+      vector<string> instruction_parts = split(instruction, ":");
+
+      if (instruction_parts.size() == 1) {
+        workflows[key].emplace_back(' ', false, -1, instruction);
         break;
-      } else {
-        char letter = p[0][0];
-        bool op = p[0][1] == '<';
-        int nums = parse_int(p[0]).value();
-        string destionation = p[1];
-        workflows[workflow].push_back({letter, op, nums, destionation});
       }
+
+      string equation = instruction_parts[0];
+      string destination = instruction_parts[1];
+
+      char argument = equation[0];
+      bool op = equation[1] == '<';
+      int numbers = parse_int(equation).value();
+
+      workflows[key].emplace_back(argument, op, numbers, destination);
     }
   }
 
-  vector<vector<int>> ratings;
-  for (string rating : split_newline(parts[1])) {
-    vector<string> rating_parts = split_comma(rating);
-    int a = stoi(rating_parts[0].substr(3));
-    int b = stoi(rating_parts[1].substr(2));
-    int c = stoi(rating_parts[2].substr(2));
-    int d = stoi(rating_parts[3].substr(2, rating_parts[3].size() - 3));
-    ratings.push_back({a, b, c, d});
+  return workflows;
+}
+
+Ratings parse_ratings(string input) {
+  Ratings ratings;
+
+  for (string rating : split_newline(input)) {
+    vector<string> rating_parts_str = split_comma(rating);
+    Rating rating_parts = filter_map(rating_parts_str, parse_long);
+    ratings.push_back(rating_parts);
   }
+
+  return ratings;
+}
+
+pair<Workflows, Ratings> parse_instructions(string path) {
+  string text = read_text(path);
+  vector<string> text_parts = split_newnewline(text);
+  Workflows workflows = parse_workflows(text_parts[0]);
+  Ratings ratings = parse_ratings(text_parts[1]);
 
   return {workflows, ratings};
 }
 
-bool get_statement(char letter, bool op, int nums, int x, int m, int a, int s) {
-  switch (letter) {
-  case 'x':
-    return op ? x < nums : x > nums;
-  case 'm':
-    return op ? m < nums : m > nums;
-  case 'a':
-    return op ? a < nums : a > nums;
-  case 's':
-    return op ? s < nums : s > nums;
-  default:
-    return false;
-  }
+bool equation_result(char argument, bool op, int numbers,
+                     const Rating &rating) {
+  int index = strchr("xmas", argument) - "xmas";
+  return op ? rating[index] < numbers : rating[index] > numbers;
 }
 
-int is_valid(const vector<int> &rating,
-             const unordered_map<string, vector<tuple<char, bool, int, string>>>
-                 &workflows) {
-  int x = rating[0];
-  int m = rating[1];
-  int a = rating[2];
-  int s = rating[3];
+bool is_valid(const Workflows &workflows, const Rating &rating) {
+  string key = "in";
 
-  string index = "in";
   while (true) {
-    vector<tuple<char, bool, int, string>> workflow = workflows.at(index);
-    for (tuple<char, bool, int, string> w : workflow) {
-      auto [letter, op, nums, dest] = w;
+    Workflow workflow = workflows.at(key);
 
-      if (nums == -1) {
-        if (dest == "A") {
-          return true;
-        } else if (dest == "R") {
-          return false;
-        }
-        index = dest;
-        break;
+    for (auto [argument, op, numbers, destination] : workflow) {
+      if (numbers != -1 && !equation_result(argument, op, numbers, rating)) {
+        continue;
       }
 
-      bool statement = get_statement(letter, op, nums, x, m, a, s);
-
-      if (statement) {
-        if (dest == "A") {
-          return true;
-        } else if (dest == "R") {
-          return false;
-        }
-        index = dest;
-        break;
+      if (destination == "A") {
+        return true;
       }
+
+      if (destination == "R") {
+        return false;
+      }
+
+      key = destination;
+      break;
     }
   }
 }
 
-int solve_p1(const vector<vector<int>> &ratings,
-             const unordered_map<string, vector<tuple<char, bool, int, string>>>
-                 &workflows) {
-  int total = 0;
-  for (vector<int> rating : ratings) {
-    bool valid = is_valid(rating, workflows);
+long solve_p1(const Ratings &ratings, const Workflows &workflows) {
+  long total = 0;
+  for (auto rating : ratings) {
+    bool valid = is_valid(workflows, rating);
     if (valid) {
       total += sum(rating);
     }
@@ -116,8 +108,74 @@ int solve_p1(const vector<vector<int>> &ratings,
   return total;
 }
 
+long solve_p2(const Ratings &ratings, const Workflows &workflows) {
+  long total = 0;
+
+  vector<int> sx = {0};
+  vector<int> sm = {0};
+  vector<int> sa = {0};
+  vector<int> ss = {0};
+
+  for (auto [_, workflow] : workflows) {
+    for (auto [c, op, in, _] : workflow) {
+      switch (c) {
+      case 'x':
+        sx.push_back(op ? in - 1 : in);
+        sx.push_back(op ? in - 1 : in);
+        break;
+      case 'm':
+        sm.push_back(op ? in - 1 : in);
+        sm.push_back(op ? in - 1 : in);
+        break;
+      case 'a':
+        sa.push_back(op ? in - 1 : in);
+        sa.push_back(op ? in - 1 : in);
+        break;
+      case 's':
+        ss.push_back(op ? in - 1 : in);
+        ss.push_back(op ? in - 1 : in);
+        break;
+      }
+    }
+  }
+
+  sx.push_back(4000);
+  sm.push_back(4000);
+  sa.push_back(4000);
+  ss.push_back(4000);
+
+  sort(sx.begin(), sx.end());
+  sort(sm.begin(), sm.end());
+  sort(sa.begin(), sa.end());
+  sort(ss.begin(), ss.end());
+
+  for (int xi = 0; xi < sx.size(); xi += 2) {
+    for (int mi = 0; mi < sm.size(); mi += 2) {
+      for (int ai = 0; ai < sa.size(); ai += 2) {
+        for (int si = 0; si < ss.size(); si += 2) {
+
+          Rating rating_last = {sx[xi + 0], sm[mi + 0], sa[ai + 0], ss[si + 0]};
+          Rating rating = {sx[xi + 1], sm[mi + 1], sa[ai + 1], ss[si + 1]};
+
+          if (is_valid(workflows, rating)) {
+            total += ((rating.at(0) - rating_last.at(0)) *
+                      (rating.at(1) - rating_last.at(1)) *
+                      (rating.at(2) - rating_last.at(2)) *
+                      (rating.at(3) - rating_last.at(3)));
+          }
+        }
+      }
+    }
+  }
+
+  return total;
+}
+
 int main(int argc, char *argv[]) {
   auto [workflows, ratings] = parse_instructions(argv[1]);
-  int p1 = solve_p1(ratings, workflows);
-  println(p1);
+
+  long p1 = solve_p1(ratings, workflows);
+  long p2 = solve_p2(ratings, workflows);
+
+  assert_print(p1, p2, 19114L, 167409079868000L);
 }
