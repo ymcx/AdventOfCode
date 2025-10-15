2681af38053420416a68d5315eaededb23c31cbe
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-15 16:26:09 +0300

    small cleanup

--- 2023/src/day20.cpp
+++ 2023/src/day20.cpp
@@ -4,110 +4,94 @@
 #include <regex>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-using Module = tuple<char, vector<string>, bool, unordered_map<string, bool>>;
+using Module =
+    tuple<char, bool, unordered_set<string>, unordered_map<string, bool>>;
 using Modules = unordered_map<string, Module>;
+using Queue = queue<tuple<string, bool, string>>;
 
 Modules parse_modules(string path) {
   Modules modules;
-  vector<string> lines = read_lines(path);
-  for (string line : lines) {
-    vector<string> parts = split(line, " -> ");
-    char type = parts[0][0];
 
-    string module = parts[0];
-    module = regex_replace(module, regex("%"), "");
-    module = regex_replace(module, regex("&"), "");
-    vector<string> connections = split_commaspace(parts[1]);
+  for (string line : read_lines(path)) {
+    vector<string> line_parts = split(line, " -> ");
 
-    modules[module] = {type, connections, false, {}};
+    string module = regex_replace(line_parts[0], regex("%|&"), "");
+    char type = line_parts[0][0];
+    bool state = false;
+    vector<string> outputs_vec = split_commaspace(line_parts[1]);
+    unordered_set<string> outputs(outputs_vec.begin(), outputs_vec.end());
+    unordered_map<string, bool> inputs;
+
+    modules[module] = {type, state, outputs, inputs};
   }
-  for (auto [module, meta] : modules) {
-    auto [type, connections, on, inputs] = meta;
-    for (string connection : connections) {
-      auto &[contype, _, _, coninputs] = modules[connection];
-      if (contype == '&') {
-        coninputs[module] = false;
-      }
+
+  for (auto [module, data] : modules) {
+    auto [_, _, outputs, _] = data;
+    for (string output : outputs) {
+      auto &[_, _, _, inputs] = modules[output];
+      inputs[module] = false;
     }
   }
 
   return modules;
 }
 
-queue<tuple<string, bool, string>> push_button(Modules &modules,
-                                               const string sendto,
-                                               const bool high, long &ls,
-                                               long &hs, const string &sender) {
+Queue zap(Modules &modules, string sender, string receiver, bool high_pulse,
+          int &lows, int &highs) {
+  auto &[type, status, outputs, inputs] = modules[receiver];
+  Queue to_zap;
 
-  auto &[type, outputs, on, inputs] = modules[sendto];
-  bool send_high;
-  bool send_at_all = true;
-
-  // print(sender);
-  // print(" -");
-  // print(high?"high":"low");
-  // print("-> ");
-  // println(sendto);
-
-  queue<tuple<string, bool, string>> tosend;
-
-  if (type == 'b') {
-    send_high = high;
-  } else if (type == '%') {
-    if (high) {
-      send_at_all = false;
-    } else {
-      send_high = on ? false : true;
-      get<2>(modules[sendto]) ^= true;
-      // on = true;
+  switch (type) {
+  case '%':
+    if (high_pulse) {
+      return to_zap;
     }
-  } else {
-    inputs[sender] = high;
-    send_high = false;
-    // println(" ");
-    for (auto [m, sent_high] : inputs) {
-      // println(m);
-      if (!sent_high) {
-        send_high = true;
-      }
-    }
-    // println(" ");
+
+    status ^= true;
+    high_pulse = status;
+    break;
+  case '&':
+    inputs[sender] = high_pulse;
+    high_pulse = any_of(inputs.begin(), inputs.end(),
+                        [](auto module) { return !module.second; });
+    break;
   }
 
-  if (send_at_all) {
-    for (string output : outputs) {
-      send_high ? ++hs : ++ls;
-      tosend.emplace(output, send_high, sendto);
-    }
+  for (string output : outputs) {
+    high_pulse ? ++highs : ++lows;
+    to_zap.emplace(output, high_pulse, receiver);
   }
-  return tosend;
+
+  return to_zap;
 }
 
 int main(int argc, char *argv[]) {
   Modules modules = parse_modules(argv[1]);
 
-  long ls = 0, hs = 0;
-  for (int i = 0; i < stoi(argv[2]); ++i) {
-    auto queue = push_button(modules, "broadcaster", false, ls, hs, "button");
-    ++ls;
+  int iterations = 1000;
+  int lows = iterations;
+  int highs = 0;
+
+  for (int i = 0; i < iterations; ++i) {
+    Queue queue = zap(modules, "button", "broadcaster", false, lows, highs);
+
     while (!queue.empty()) {
-      auto [soutput, ssend_high, ssender] = queue.front();
+      auto [receiver, high_pulse, sender] = queue.front();
       queue.pop();
-      auto newqueue =
-          push_button(modules, soutput, ssend_high, ls, hs, ssender);
-      while (!newqueue.empty()) {
-        queue.push(newqueue.front());
-        newqueue.pop();
+
+      Queue queue_new = zap(modules, sender, receiver, high_pulse, lows, highs);
+      while (!queue_new.empty()) {
+        queue.push(queue_new.front());
+        queue_new.pop();
       }
     }
   }
 
-  // println(ls);
-  // println(hs);
-  println(ls * hs);
-  // }
+  int p1 = lows * highs;
+  println(p1);
 }
