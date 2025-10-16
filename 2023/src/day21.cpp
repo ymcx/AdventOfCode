ec533f81be9e60a8f85fc3624fcb405bf934b47c
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-16 20:05:02 +0300

    2023, day21b

--- 2023/src/day21.cpp
+++ 2023/src/day21.cpp
@@ -1,79 +1,103 @@
 #include "misc/io.h"
 #include "misc/point.h"
-#include "misc/vector.h"
 #include <queue>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-bool is_div(int i) { return i % 2 == 0; }
-int main(int argc, char *argv[]) {
-  vector<string> lines = read_lines(argv[1]);
-  Point dimensions = Point(lines.size(), lines[0].size());
+tuple<unordered_set<Point>, Point, Point> parse_map(string path) {
+  vector<string> lines = read_lines(path);
+
   unordered_set<Point> rocks;
   Point start;
+  Point dimensions = Point(lines.size(), lines[0].size());
 
   for (int y = 0; y < dimensions.y; ++y) {
     for (int x = 0; x < dimensions.x; ++x) {
       Point point = Point(y, x);
-      if (lines[y][x] == '#') {
+
+      switch (lines[y][x]) {
+      case '#':
         rocks.insert(point);
-      }
-      if (lines[y][x] == 'S') {
+        break;
+      case 'S':
         start = point;
+        break;
       }
     }
   }
 
-  queue<pair<Point, int>> positions;
-  unordered_map<Point, int> already_travelled;
-  positions.push({start, stoi(argv[2])});
-  while (!positions.empty()) {
-    auto [pos, left] = positions.front();
-    positions.pop();
+  return {rocks, start, dimensions};
+}
 
-    if (left < 0 || rocks.contains(pos) ||
-        (already_travelled.contains(pos) && already_travelled[pos] >= left)) {
+unordered_map<Point, int> get_visited(const unordered_set<Point> &rocks,
+                                      Point start, Point dimensions) {
+  unordered_map<Point, int> visited;
+  queue<pair<Point, int>> queue;
+  queue.emplace(start, 0);
+
+  while (!queue.empty()) {
+    auto [point, steps] = queue.front();
+    queue.pop();
+
+    if (rocks.contains(point) || visited.contains(point) ||
+        !point.is_valid(dimensions)) {
       continue;
     }
 
-    already_travelled[pos] = left;
+    visited[point] = steps;
 
-    if (left <= 0) {
-      continue;
-    }
-
-    left -= 1;
-
-    positions.push({pos + Point(1, 0), left});
-    positions.push({pos + Point(-1, 0), left});
-    positions.push({pos + Point(0, 1), left});
-    positions.push({pos + Point(0, -1), left});
+    queue.emplace(Point(point.y - 1, point.x), steps + 1);
+    queue.emplace(Point(point.y + 1, point.x), steps + 1);
+    queue.emplace(Point(point.y, point.x - 1), steps + 1);
+    queue.emplace(Point(point.y, point.x + 1), steps + 1);
   }
 
-  for (int y = 0; y < dimensions.y; ++y) {
-    for (int x = 0; x < dimensions.x; ++x) {
-      Point point = Point(y, x);
-      if (rocks.contains(point)) {
-        print("#");
-      } else if (already_travelled.contains(point)) {
-        if (already_travelled[point] >= 10) {
-          print(to_string(already_travelled[point])[0]);
+  return visited;
+}
 
-        } else {
-          print(already_travelled[point]);
-        }
-      } else {
-        print(" ");
-      }
-    }
-    println();
+tuple<long, long, long, long>
+get_squares(const unordered_map<Point, int> &visited, Point dimensions) {
+  long even_corners = 0;
+  long odd_corners = 0;
+  long even_full = 0;
+  long odd_full = 0;
+
+  for (auto [_, value] : visited) {
+    even_corners += value % 2 == 0 && value > dimensions.x / 2;
+    odd_corners += value % 2 == 1 && value > dimensions.x / 2;
+    even_full += value % 2 == 0;
+    odd_full += value % 2 == 1;
   }
 
-  vector<int> vals = values(already_travelled);
-  vals = filter(vals, is_div);
-  int p1 = vals.size();
-  println(p1);
+  return {even_corners, odd_corners, even_full, odd_full};
+}
+
+// https://github.com/villuna/aoc23
+long solve(tuple<long, long, long, long> squares, Point dimensions, bool p2) {
+  auto [even_corners, odd_corners, even_full, odd_full] = squares;
+
+  long steps = p2 ? 26501365 : 64;
+  long n = steps / dimensions.x;
+
+  long a = (n + 1) * (n + 1) * (p2 ? odd_full : even_full);
+  long b = n * n * (p2 ? even_full : odd_full);
+  long c = (n + 1) * (p2 ? odd_corners : even_corners);
+  long d = n * (p2 ? even_corners : odd_corners);
+
+  return a + b - c + d;
+}
+
+int main(int argc, char *argv[]) {
+  auto [rocks, start, dimensions] = parse_map(argv[1]);
+  unordered_map<Point, int> visited = get_visited(rocks, start, dimensions);
+  tuple<long, long, long, long> squares = get_squares(visited, dimensions);
+
+  long p1 = solve(squares, dimensions, false);
+  long p2 = solve(squares, dimensions, true);
+
+  assert_print(p1, p2, 3858L, 636350496972143L);
 }
