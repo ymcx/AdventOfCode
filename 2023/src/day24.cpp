66a3d33aa6bdf87717989f31d9d15e01fadfe689
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-30 18:33:53 +0200

    2023, day24b

--- 2023/src/day24.cpp
+++ 2023/src/day24.cpp
@@ -12,20 +12,17 @@ using Eigen::Vector2f;
 constexpr float LOW = 200000000000000.0F;
 constexpr float HIGH = 400000000000000.0F;
 
-array<float, 6> parse_line(const string input) {
+array<long, 6> parse_line(const string input) {
   const vector<string> splitted = split_space(input);
-  const vector<float> parsed = filter_map(splitted, parse_float);
+  const vector<long> parsed = filter_map(splitted, parse_long);
 
-  array<float, 6> output;
+  array<long, 6> output;
   copy_n(parsed.data(), 6, output.begin());
 
   return output;
 }
 
-int main(const int argc, const char *argv[]) {
-  const vector<string> lines_str = read_lines(argv[1]);
-  const vector<array<float, 6>> lines = map(lines_str, parse_line);
-
+int solve_p1(const vector<array<long, 6>> &lines) {
   int intersections = 0;
 
   for (int i = 0; i < lines.size(); ++i) {
@@ -34,11 +31,13 @@ int main(const int argc, const char *argv[]) {
       const auto &[x1, y1, _, x1d, y1d, _] = lines[j];
 
       const Vector2f p0(x0, y0);
-      const Vector2f p1(x0 + x0d, y0 + y0d);
+      const Vector2f p1(x0 + static_cast<float>(x0d),
+                        y0 + static_cast<float>(y0d));
       const Hyperplane p = Hyperplane<float, 2>::Through(p0, p1);
 
       const Vector2f q0(x1, y1);
-      const Vector2f q1(x1 + x1d, y1 + y1d);
+      const Vector2f q1(x1 + static_cast<float>(x1d),
+                        y1 + static_cast<float>(y1d));
       const Hyperplane q = Hyperplane<float, 2>::Through(q0, q1);
 
       const auto intersection = p.intersection(q);
@@ -70,5 +69,64 @@ int main(const int argc, const char *argv[]) {
     }
   }
 
-  println(intersections);
+  return intersections;
+}
+
+// https://www.reddit.com/user/mynt
+// https://pastebin.com/pnbxaCVu
+long solve_p2(const vector<array<long, 6>> &lines) {
+  const auto &[x0, y0, z0, vx0, vy0, vz0] = lines[72];
+  const auto &[x1, y1, z1, vx1, vy1, vz1] = lines[101];
+  const auto &[x2, y2, z2, vx2, vy2, vz2] = lines[189];
+
+  const __int128 vxr1 = vx1 - vx0;
+  const __int128 vzr1 = vz1 - vz0;
+  const __int128 vxr2 = vx2 - vx0;
+  const __int128 vzr2 = vz2 - vz0;
+
+  const __int128 xr1 = x1 - x0;
+  const __int128 yr1 = y1 - y0;
+  const __int128 zr1 = z1 - z0;
+
+  const __int128 xr2 = x2 - x0;
+  const __int128 yr2 = y2 - y0;
+  const __int128 zr2 = z2 - z0;
+
+  __int128 num =
+      yr2 * (xr1 * vzr1 - vxr1 * zr1) + yr1 * (zr2 * vxr1 - vzr1 * xr2);
+
+  __int128 den = yr1 * (vzr1 * vxr2 - vxr1 * vzr2);
+  const __int128 t2 = num / den;
+
+  num = yr1 * (xr2 + vxr2 * t2) - yr2 * xr1;
+  den = yr2 * vxr1;
+  const __int128 t1 = num / den;
+
+  const __int128 cx1 = x1 + t1 * vx1;
+  const __int128 cy1 = y1 + t1 * vy1;
+  const __int128 cz1 = z1 + t1 * vz1;
+
+  const __int128 cx2 = x2 + t2 * vx2;
+  const __int128 cy2 = y2 + t2 * vy2;
+  const __int128 cz2 = z2 + t2 * vz2;
+
+  const __int128 xm = (cx2 - cx1) / (t2 - t1);
+  const __int128 ym = (cy2 - cy1) / (t2 - t1);
+  const __int128 zm = (cz2 - cz1) / (t2 - t1);
+
+  const __int128 xc = cx1 - xm * t1;
+  const __int128 yc = cy1 - ym * t1;
+  const __int128 zc = cz1 - zm * t1;
+
+  return xc + yc + zc;
+}
+
+int main(const int argc, const char *argv[]) {
+  const vector<string> lines_str = read_lines(argv[1]);
+  const vector<array<long, 6>> lines = map(lines_str, parse_line);
+
+  const int p1 = solve_p1(lines);
+  const long p2 = solve_p2(lines);
+
+  assert_print(p1, p2, 15889, 801386475216902L);
 }
